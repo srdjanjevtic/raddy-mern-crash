315184d11c9b2ad57fa41bfd8003547c9097c414
srdjanjevtic <srdjanjevticle@gmail.com>
2023-10-20 15:44:19 +0200

    backend done

--- server/controllers/noteControllers.js
+++ server/controllers/noteControllers.js
@@ -22,6 +22,27 @@ export const createNote = async (req, res) => {
   }
 };
 
+// @desc    Update the note
+// @route   PUT  /api/notes/:id
+// @access  Public
+export const updateNote = async (req, res) => {
+  try {
+    const noteId = req.params.id;
+    const { title, description, category } = req.body;
+    const data = await Note.findByIdAndUpdate(noteId, {
+      title,
+      description,
+      category,
+    });
+    if (!data) {
+      throw new Error("An error occurred");
+    }
+    return res.status(201).json(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 // @desc    Get all notes
 // @route   GET  /api/notes
 // @access  Public
@@ -34,4 +55,36 @@ export const getAllNotes = async (req, res) => {
     throw new Error("There is no notes in DB");
   }
   return res.status(200).json(data);
+};
+
+// @desc    Get the note
+// @route   GET  /api/notes/:id
+// @access  Public
+export const getNote = async (req, res) => {
+  try {
+    const noteId = req.params.id;
+    const data = await Note.findById(noteId);
+    if (!data) {
+      throw new Error({ message: "There is no note with provided id" });
+    }
+    return res.status(200).json(data);
+  } catch (error) {
+    res.status(404).json(error.message);
+  }
+};
+
+// @desc    Delete note
+// @route   DELETE  /api/notes/:id
+// @access  Public
+export const deleteNote = async (req, res) => {
+  try {
+    const noteId = req.params.id;
+    const data = await Note.findByIdAndDelete(noteId);
+    if (!data) {
+      throw new Error({ message: "There is no note with provided id" });
+    }
+    return res.status(200).json(data);
+  } catch (error) {
+    res.status(404).json(error.message);
+  }
 };
--- server/routes/noteRoutes.js
+++ server/routes/noteRoutes.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { createNote, getAllNotes } from "../controllers/noteControllers.js";
+import {
+  createNote,
+  getAllNotes,
+  getNote,
+  updateNote,
+  deleteNote,
+} from "../controllers/noteControllers.js";
 
 const router = express.Router();
 
 router.post("/", createNote);
 router.get("/", getAllNotes);
+router.get("/:id", getNote);
+router.put("/:id", updateNote);
+router.delete("/:id", deleteNote);
 
 export default router;
